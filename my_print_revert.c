928e947be013725bd983287b8755853872818820
Val8063 <111076646+Val8063@users.noreply.github.com>
2024-04-17 12:15:36 +0000

    ex 3

---
+++ my_print_revert.c
@@ -0,0 +1,20 @@
+void print_char(char c);
+
+void my_print_revert(char *toRevert)
+{
+    int iter = 0;
+    while (toRevert[iter] != '\0')
+    {
+        iter++;
+    }
+    for (int i = iter; i >= 0; i--)
+    {
+        print_char(toRevert[i]);
+    }    
+}
+
+int main(void)
+ {
+    my_print_revert("codingclub");
+    return 0;
+ }
